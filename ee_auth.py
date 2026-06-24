792e2fa19f8f8751e50d3d13fdff133b4c672901
Laiba Khan <152780287+Laibaa-Khan@users.noreply.github.com>
2026-06-24 11:10:08 +0500

    Update ee_auth.py

--- ee_auth.py
+++ ee_auth.py
@@ -1,21 +1,29 @@
 import ee
-import json
-import os
-
+import streamlit as st
 from google.oauth2 import service_account
 
-service_account_info = json.loads(
-    os.environ["EE_KEY"]
+# Load service account credentials from Streamlit Secrets
+service_account_info = {
+    "type": st.secrets["type"],
+    "project_id": st.secrets["project_id"],
+    "private_key_id": st.secrets["private_key_id"],
+    "private_key": st.secrets["private_key"],
+    "client_email": st.secrets["client_email"],
+    "client_id": st.secrets["client_id"],
+    "auth_uri": st.secrets["auth_uri"],
+    "token_uri": st.secrets["token_uri"],
+    "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
+    "client_x509_cert_url": st.secrets["client_x509_cert_url"],
+    "universe_domain": "googleapis.com"
+}
+
+# Create credentials object
+credentials = service_account.Credentials.from_service_account_info(
+    service_account_info
 )
 
-credentials = (
-    service_account.Credentials
-    .from_service_account_info(
-        service_account_info
-    )
-)
-
+# Initialize Earth Engine
 ee.Initialize(
     credentials,
-    project="bubbly-sentinel-486808-v7"
+    project=st.secrets["project_id"]
 )
