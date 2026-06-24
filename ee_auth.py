cbaadcd5a616766a9ebe774c44b18274b164f299
Laiba Khan <152780287+Laibaa-Khan@users.noreply.github.com>
2026-06-24 11:39:17 +0500

    Update ee_auth.py

--- ee_auth.py
+++ ee_auth.py
@@ -2,8 +2,14 @@ import ee
 import streamlit as st
 from google.oauth2 import service_account
 
+SCOPES = [
+    "https://www.googleapis.com/auth/earthengine",
+    "https://www.googleapis.com/auth/cloud-platform",
+]
+
 credentials = service_account.Credentials.from_service_account_info(
-    dict(st.secrets)
+    dict(st.secrets),
+    scopes=SCOPES
 )
 
 ee.Initialize(
